f95c1a86aa895fd83fca12c2333a049937e08fe7
SukumDecha <ninemaster12gt@gmail.com>
2024-06-09 17:19:32 +0700

    add database

--- src/main/java/me/sit/dev/ui/impl/AdminUI.java
+++ src/main/java/me/sit/dev/ui/impl/AdminUI.java
@@ -27,38 +27,25 @@ public class AdminUI extends BaseUI {
     }
 
     public void showAdminMenu() {
-        System.out.println("\n\t\t\t--- Admin Menu ---\n");
-        System.out.println("1. Add a new restaurant");
-        System.out.println("2. Update a restaurant");
-        System.out.println("3. Delete a restaurant");
-        System.out.println("4. Show all restaurants");
-        System.out.println("5. Exit from program");
-        System.out.print("[!] Please enter 1|2|3|4|5 : ");
-        while (!sc.hasNext("[1|2|3|4|5]")) {
-            System.out.print("[!] Please try again. (Enter 1|2|3|4|5) : ");
-            sc.next();
-        }
-        int selected = sc.nextInt();
-        switch (selected) {
-            case 1:
-                addRestaurant();
-                break;
-            case 2:
-                updateRestaurant();
-                break;
-            case 3:
-                deleteRestaurant();
-                break;
-            case 4:
-                showAllRestaurants();
-                break;
-            case 5:
-                System.out.println("Exiting from the program...");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid input. Please try again.");
-                showAdminMenu();
+        while (true) {
+            System.out.println("--- Select Entity to edit ---");
+            System.out.println("1. User");
+            System.out.println("2. Restaurant");
+            System.out.println("----------------------------");
+            System.out.print("Enter your choice: ");
+            int entityChoice = sc.nextInt();
+            sc.nextLine();  // Consume newline
+
+            switch (entityChoice) {
+                case 1:
+                    editUser(sc);
+                    break;
+                case 2:
+                    editRestaurant(sc);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
         }
     }
 
